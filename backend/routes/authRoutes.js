081950f048ee995058fea9740ced7a37d97dcbe7
sivaprakasam-07 <tamilmanisivaprakasam5@gmail.com>
2025-07-06 11:00:49 +0530

    cleared commited code

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,27 +1,3 @@
-// const express = require('express');
-// const router = express.Router();
-
-// // Test route
-// router.get('/test', (req, res) => {
-//     res.json({ 
-//         message: 'Auth routes working!',
-//         timestamp: new Date().toISOString()
-//     });
-// });
-
-// // Login route
-// router.post('/login', (req, res) => {
-//     res.json({ message: 'Login endpoint - Coming soon!' });
-// });
-
-// // Register route
-// router.post('/register', (req, res) => {
-//     res.json({ message: 'Register endpoint - Coming soon!' });
-// });
-
-// module.exports = router;
-
-
 const express = require('express');
 const router = express.Router();
 const { registerUser, loginUser } = require('../controllers/authController');
